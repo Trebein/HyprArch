257ce7b42c50d2faca3ca12ff8186fa94e5c147d
Trebein <154022424+Trebein@users.noreply.github.com>
2025-05-30 09:59:07 +0300

    Update InScript.sh

--- InScript.sh
+++ InScript.sh
@@ -5,7 +5,26 @@ PACKAGES_MAIN=(nano dmidecode xarchiver thunar fastfetch flatpack git mc meson f
 PACKAGES_FONTS=(ttf-font-awesome otf-font-awesome ttf-jetbrains-mono)
 PACKAGES_FISH=(fish pkgfile ttf-dejavu powerline)
 PACKAGES_THEME=(nwg-look papirus-icon-theme)
+PACKAGES_BLUETOOTH=(blueman bluez bluez-utils bluez-deprecated-tools tlp)
+PACKAGES_PIPEWIRE=(pipewire pipewire-pulse pipewire-jack lib32-pipewire gst-plugin-pipewire wireplumber)
+PACKAGES_NETWORK=(dhcpcd)
+PACKAGES_GAME=(steam-native-runtime lutris wine)
 
+FLATPAK_APPS=(
+    org.chromium.Chromium
+    com.brave.Browser
+    com.discordapp.Discord
+    me.kozec.syncthingtk
+    org.telegram.desktop
+    md.obsidian.Obsidian
+    # io.crow_translate.CrowTranslate
+    com.librumreader.librum
+    com.obsproject.Studio
+    org.keepassxc.KeePassXC
+    com.nextcloud.desktopclient.nextcloud
+    io.github.shiftey.Desktop
+    # de.shorsh.discord-screenaudio
+)
 
 
 echo -e "\e[1;32m
@@ -54,6 +73,36 @@ yay -S hyprshot github-desktop --noconfirm
 echo -e "\e[1;32m
 =======================================================
 =======================================================
+                 Настройка Bluetooth
+=======================================================
+=======================================================
+\e[0m"
+sudo pacman -S --noconfirm "${PACKAGES_BLUETOOTH[@]}"
+sudo systemctl enable --now tlp
+
+echo -e "\e[1;32m
+=======================================================
+=======================================================
+             Настройка звука (PipeWire)
+=======================================================
+=======================================================
+\e[0m"
+sudo pacman -S --noconfirm "${PACKAGES_PIPEWIRE[@]}"
+# systemctl --user enable --now pipewire pipewire.socket pipewire-pulse wireplumber
+
+echo -e "\e[1;32m
+=======================================================
+=======================================================
+                  Настройка сети
+=======================================================
+=======================================================
+\e[0m"
+sudo pacman -S --noconfirm "${PACKAGES_NETWORK[@]}"
+# sudo systemctl enable --now dhcpcd
+
+echo -e "\e[1;32m
+=======================================================
+=======================================================
                   Установка шрифтов
 =======================================================
 =======================================================
@@ -76,7 +125,6 @@ echo -e "\e[1;32m
 =======================================================
 =======================================================
 \e[0m"
-
 sudo pacman -S --noconfirm "${PACKAGES_THEME[@]}"
 git clone https://github.com/vinceliuice/Graphite-gtk-theme
 cd Graphite-gtk-theme/
@@ -106,7 +154,6 @@ echo -e "\e[1;32m
 =======================================================
 =======================================================
 \e[0m"
-
 cp -f ~/HyprArch/config-files/BASH/.bashrc ~/
 mkdir -p ~/.config/hypr/
 cp -f ~/HyprArch/config-files/hypr/hyprland.conf ~/.config/hypr/
@@ -131,9 +178,22 @@ echo -e "\e[1;32m
 =======================================================
 \e[0m"
 gsettings set org.gnome.desktop.interface icon-theme Papirus-Dark
-gsettings get org.gnome.desktop.interface icon-theme # вывод текущих иконок
+# gsettings get org.gnome.desktop.interface icon-theme # вывод текущих иконок
 gsettings set org.gnome.desktop.interface gtk-theme Graphite-Dark
-gsettings get org.gnome.desktop.interface gtk-theme # вывод текущей темы
+# gsettings get org.gnome.desktop.interface gtk-theme # вывод текущей темы
+
+
+echo -e "\e[1;32m
+=======================================================
+=======================================================
+            Подключение и установка Snap
+=======================================================
+=======================================================
+\e[0m"
+# git clone https://aur.archlinux.org/snapd.git
+# cd snapd && makepkg -si --noconfirm && cd ~
+# sudo systemctl enable --now snapd.socket
+# sudo snap install tradingview
 
 echo -e "\e[1;32m
 =======================================================
@@ -143,9 +203,6 @@ echo -e "\e[1;32m
 =======================================================
 \e[0m"
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-
-
-
 
 echo -e "\e[1;32m
 =======================================================
@@ -160,9 +217,9 @@ echo -e "\e[1;32m
 =======================================================
 =======================================================
 \e[0m"
-flatpak install flathub org.telegram.desktop
-flatpak install flathub com.brave.Browser
-flatpak install flathub md.obsidian.Obsidian
+for app in "${FLATPAK_APPS[@]}"; do
+    flatpak install -y flathub "$app"
+done
 
 echo -e "\e[1;32m
 =======================================================
